927e2405577c4b390bb17b3d8eb7681b7fdadca2
JobCorteF <143737733+JobCorteF@users.noreply.github.com>
2025-02-13 16:44:46 -0600

    Update MultipleX.py

--- MultipleX.py
+++ MultipleX.py
@@ -7,6 +7,6 @@ y =np.array([,,,,,,,])
 model = LinearRegression()
 model.fit(X, y)
 
-prediccion = model.predict([[6,50,60]])
+prediccion = model.predict([[,,]])
 
-print(f"Nota final: {prediccion[0]} miles de dólares")   
+print(f"Lorem: {prediccion[0]} ipsum")   
