24e8ce7655049d612a75643f6d50fffab1e5cf96
JobCorteF <143737733+JobCorteF@users.noreply.github.com>
2025-02-13 16:42:24 -0600

    Update MultipleX.py

--- MultipleX.py
+++ MultipleX.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-X =np.array([[5,80,70], [6,85,75], [7,90,80], [8,95,85], [9,100,90], [10,100,92], [11,95,88], [12,90,85]])
-y =np.array([75,80,85,90,95,98,92,89])
+X =np.array([[,,], [,,], [,,], [,,], [,,], [,,], [,,], [,,]])
+y =np.array([,,,,,,,])
 
 model = LinearRegression()
 model.fit(X, y)
